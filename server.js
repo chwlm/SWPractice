76aaf4ca1bcfed6bad55afbdb732fb070f6c0fdb
chawin <l.chawin@outlook.com>
2024-02-26 21:01:47 +0700

    Login system

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 //Load env vars
@@ -10,12 +11,18 @@ connectDB();
 
 // Route file
 const hospitals = require ('./routes/hospitals');
+const auth = require('./routes/auth');
+
 
 const app=express();
 
-//Body parser
+// Body parser
 app.use(express.json());
-app.use('/api/v1/hospitals',hospitals)
+app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
+
+// Cookie parser
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000
 
